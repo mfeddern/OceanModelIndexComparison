bd1ec9ada6b0c33d6b9cb6bc0d82b4f8c5812dd3
Megan Feddern <mfeddern@alaska.edu>
2025-07-09 12:28:06 -0700

    dataframe and csv of monthly data

--- OcModels/Src/GLORYS_Processing/00_GLORYS_download.R
+++ OcModels/Src/GLORYS_Processing/00_GLORYS_download.R
@@ -32,7 +32,7 @@ library(CopernicusMarine)
 # Copernicus exe path ##########################################################
 
 path_copernicusmarine =  "/Users/megan.feddern/miniforge3/envs/copernicusmarine/bin/copernicusmarine"
-
+out_dir<-"Data/GLORYS_Processing/"
 # SET LAT-LON, DEPTH & DATES #########################################################
 
 lon = list(-130, -116)  # lon_min, lon_max
@@ -176,183 +176,24 @@ for(i in 1:2){
                     " --variable vo", "-o", out_dir, "-f", filename, 
                     "--force-download", sep = " ")
   system(command, intern = TRUE)
-}
-
-
-
-
-################################################################################
-# DAILY TIME SERIES DOWNLOADS ##################################################
-################################################################################
-
-################################################################################
-# LONGSSHORE - CROSS-SHELF - TEMPERATURE #######################################
-################################################################################
-
-# download daily data by year otherwise the files get too large to load.
-# many depth layers per year
-# loop because the files are large
-# don't really use...except for temp. 
-
-# automatically set date ranges for downloads
-reanalysis_end_year = as.numeric(substring(reanalysis_end_date,1,4))
-start_year = as.numeric(substring(reanalysis_start_date,1,4))
-end_year = as.numeric(substring(interim_end_date,1,4))
-n = reanalysis_end_year - start_year
-n
-
-#reanalysis first year ####
-date_range = data.frame(
-  date_min = reanalysis_start_date,
-  date_max = as.numeric(paste0(start_year,1231)),
-  dataset = "cmems_mod_glo_phy_my_0.083deg_P1D-m")
-
-# reanalysis full years ####
-# if(n>1){
-for(i in 1:n){
-  next_year = start_year+i
-  date_range[i+1,"date_min"] = as.numeric(paste0(next_year,'0101'))
-  date_range[i+1,"date_max"] = as.numeric(paste0(next_year,'1231'))
-  date_range[i+1,"dataset"] = "cmems_mod_glo_phy_my_0.083deg_P1D-m"
-}
-# }
-# replace last date_max with interim value
-date_range[n+1,'date_max'] <- reanalysis_end_date
-
-# add interim partial year
-date_range[n+2, "date_min"] <- interim_start_date 
-date_range[n+2, "date_max"] <- as.numeric(paste0(substring(interim_start_date,1,4),"1231"))
-date_range[n+2, "dataset"]  <- "cmems_mod_glo_phy_myint_0.083deg_P1D-m" 
-
-# full interim years
-first_full_year = as.numeric(substring(interim_start_date,1,4))+1
-int_full_start = as.numeric(paste0(first_full_year,'0101'))
-int_full_end = as.numeric(paste0(first_full_year,'1231'))
-
-
-full_int = data.frame(
-  date_min = int_full_start,
-  date_max = int_full_end ,
-  dataset = "cmems_mod_glo_phy_myint_0.083deg_P1D-m" )
-n = end_year - first_full_year
-
-for(i in 1:n){
-  next_year = first_full_year + i
-  full_int[i+1,"date_min"] = as.numeric(paste0(next_year,'0101'))
-  full_int[i+1,"date_max"] = as.numeric(paste0(next_year,'1231'))
-  full_int[i+1,"dataset"] =  "cmems_mod_glo_phy_myint_0.083deg_P1D-m"
-}
-
-full_int[n+1,'date_max'] <- interim_end_date
-
-date_range = rbind(date_range, full_int)
-
-date_range
-
-# download LST, CST, BT ########################################################
-
-# for(i in 1:nrow(date_range)){ 
-for(i in 1:1){ 
   
-  date_min = ymd(date_range[i,'date_min'])
-  date_max = ymd(date_range[i,'date_max'])
-  dataset_id = date_range[i,"dataset"]
+  ## monthly temp 
+  filename = paste0("glorys-monthly-thetao-",date_min,"-",date_max,"-",depth[2],"m.nc")
+  filename
+  # download command 
   
-  # # cross shelf ####
-  # filename = paste0("glorys-daily-crosshelf-eastward-",
-  #                   date_min,"-",date_max,"-",depth[2],"m.nc")
-  # print(filename)
-  # # download command 
-  # 
-  # command <- paste (path_copernicusmarine, " subset -i", dataset_id,                    
-  #              "-x", lon[1], "-X", lon[2],                  
-  #              "-y", lat[1], "-Y", lat[2],
-  #              "-t", date_min, "-T", date_max,
-  #              "-z", depth[1], "-Z", depth[2],                    
-  #              " --variable uo", 
-  #              "-o", out_dir, "-f", filename, 
-  #              "--force-download", sep = " ")
-  # system(command, intern = TRUE)
-  # 
-  # # longshore ###
-  # filename = paste0("glorys-daily-longshore-northward-",
-  #                   date_min,"-",date_max,"-",depth[2],"m.nc")
-  # 
-  # command <- paste (path_copernicusmarine, " subset -i", dataset_id,                    
-  #                   "-x", lon[1], "-X", lon[2],                  
-  #                   "-y", lat[1], "-Y", lat[2],
-  #                   "-t", date_min, "-T", date_max,
-  #                   "-z", depth[1], "-Z", depth[2],                    
-  #                   " --variable vo", 
-  #                   "-o", out_dir, "-f", filename, 
-  #                   "--force-download", sep = " ")
-  # system(command, intern = TRUE)
-  
-  # temperature ####
-  filename = paste0("glorys-daily-watercolumn-temp-",
-                    date_min,"-",date_max,"-",depth[2],"m.nc")
-  print(filename)
-  command <- paste (path_copernicusmarine, " subset -i", dataset_id,                    
-                    "-x", lon[1], "-X", lon[2],                  
-                    "-y", lat[1], "-Y", lat[2],
-                    "-t", date_min, "-T", date_max,
-                    "-z", depth[1], "-Z", depth[2],                    
-                    " --variable thetao", 
-                    "-o", out_dir, "-f", filename,sep = " ")
-  system(command, intern = TRUE)
-  
-  
-}
-
-# product transtion year #######################################################
-# will need to delete a file
-
-date_min = ymd(date_range[i,'date_min'])
-date_max = ymd(date_range[i,'date_max'])
-dataset_id = date_range[i,"dataset"]
-
-filename = paste0("glorys-daily-watercolumn-temp-",
-                  date_min,"-",date_max,"-",depth[2],"m.nc")
-print(filename)
-command <- paste (path_copernicusmarine, " subset -i", dataset_id,                    
-                  "-x", lon[1], "-X", lon[2],                  
-                  "-y", lat[1], "-Y", lat[2],
-                  "-t", date_min, "-T", date_max,
-                  "-z", depth[1], "-Z", depth[2],                    
-                  " --variable thetao", 
-                  "-o", out_dir, "-f", filename,sep = " ")
-system(command, intern = TRUE)
-
-
-# sea surface height ###########################################################
-
-date_range = data.frame(matrix(data=c(
-  19930101, reanalysis_end_date, "cmems_mod_glo_phy_my_0.083deg_P1D-m",
-  interim_start_date, interim_end_date, "cmems_mod_glo_phy_myint_0.083deg_P1D-m"),
-  nrow=2, ncol=3, byrow=T))
-colnames(date_range) = c('date_min', 'date_max', 'dataset')
-
-
-date_range
-
-for(i in 1:nrow(date_range)){
-  date_min = ymd(date_range[i,'date_min'])
-  date_max = ymd(date_range[i,'date_max'])
-  dataset_id = date_range[i,"dataset"]
-  
-  
-  filename = paste0("glorys-daily-sea-surface-height-",
-                    date_min,"-",date_max,"-",depth[2],"m.nc")
   command <- paste (path_copernicusmarine, " subset -i", dataset_id,                    
                     "-x", lon[1], "-X", lon[2],                  
                     "-y", lat[1], "-Y", lat[2],
                     "-t", date_min, "-T", date_max,
                     # "-z", depth[1], "-Z", depth[2],                    
-                    " --variable zos", 
-                    "-o", out_dir, "-f", filename, 
+                    " --variable thetao", "-o", out_dir, "-f", filename, 
                     "--force-download", sep = " ")
   system(command, intern = TRUE)
 }
+
+
+
 
 
 # quick look at output files ###################################################
